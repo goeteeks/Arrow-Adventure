b5c86079524a8378dbd9c9bcc79781401df95e96
jjrobertson14 <johnathon.j.robertson@gmail.com>
2018-03-10 21:23:22 -0500

    initial work

---
+++ Assets/Scripts/GameManagement/SwapParts.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwapParts : MonoBehaviour {
+
+	private List<string> bodies = new List<string>();
+	private List<string> tips = new List<string>();
+	private List<string> tails = new List<string>();
+
+	private string[] bodiesArray;
+	private string[] tipsArray;
+	private string[] tailsArray;
+
+	private string curBody;
+	private string curTip;
+	private string curTail;
+
+	private PlayerManager pm;
+
+	// Use this for initialization
+	void Start () {
+		bodies.Add ("normal");
+		bodies.Add ("wood");
+		bodies.Add ("metal");
+
+		tips.Add ("normal");
+		tips.Add ("wood");
+		tips.Add ("metal");
+
+		tails.Add ("normal");
+		tails.Add ("wood");
+		tails.Add ("metal");
+
+		bodiesArray = bodies.ToArray ();
+		tipsArray = tips.ToArray ();
+		tailsArray = tails.ToArray ();
+
+		curBody = bodiesArray [0];
+		curTip = tipsArray [0];
+		curTail = tailsArray [0];
+
+		pm = GameObject.FindWithTag ("Player").GetComponent <PlayerManager> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void swapPart (string[] partsList, int index){
+		if (index > partsList.Length) {
+			Debug.LogError ("Given index is out of range!");
+		} else if (index < 0) {
+			Debug.LogError ("Given Index cannot be negative!");
+		} else {
+			
+		}
+	}
+}
---
+++ Assets/Scripts/GameManagement/TailType.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class TailType : MonoBehaviour
+{
+	private GameObject player;
+	private PlayerManager playerManager;
+	private GameObject partSwapperObject;
+	private SwapParts partSwapper;
+	public string[] tails;
+	public string currentTail;
+	private string prevTail;
+	public float metalDelay = .25f;
+
+	/* the current tail should be able to be set
+	*/
+
+	// Use this for initialization
+	void Start ()
+	{
+		partSwapperObject = (GameObject) GameObject.FindObjectOfType(typeof(SwapParts));
+		partSwapper = partSwapperObject.GetComponent <SwapParts> ();
+		tails = partSwapper.tailsArray;
+		
+		player = GameObject.FindGameObjectWithTag ("Player");
+		playerManager = player.GetComponent <PlayerManager> ();
+	}
+
+	void selectType (string type)
+	{
+		if (tails.Contains(type))
+		{
+			currentTail = type;
+			if (currentTail == "metal")
+			{
+				print("setting metal delay");
+				try
+				{
+					playerManager.setLaunchDelay(metalDelay);
+				}
+				catch (Exception e)
+				{
+					print(e.Message);
+				}
+				print("after that");
+			}
+		}
+		else
+		{
+			//not sure if hours print there
+			print("tail type parameter given is not valid");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (prevTail != currentTail)
+		{
+			prevTail = currentTail;
+			selectType("normal");
+			selectType(prevTail);
+		}
+	}
+}
